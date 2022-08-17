86674eddd63d2c17032bfd4aefcbc25428397084
Fran Fernandez <zoomff@gmail.com>
2022-08-17 17:56:59 -0300

    Adding allure reports.

--- src/test/groovy/support/steps/general.groovy
+++ src/test/groovy/support/steps/general.groovy
@@ -3,7 +3,6 @@ package support.steps
 import common.Endpoint
 import common.HttpConnector
 import io.cucumber.groovy.EN
-
 this.metaClass.mixin(EN)
 
 Given(~/^I do a get to the "(.*)" endpoint$/) { String endpointName ->
@@ -12,5 +11,10 @@ Given(~/^I do a get to the "(.*)" endpoint$/) { String endpointName ->
 
 Then(~/^the returned status code is: "(\d{3})"$/) { String expectedCodeReturned ->
     String lastResponseStatusCode = HttpConnector.getResponse().responseCode
-    assert lastResponseStatusCode == expectedCodeReturned
+    assert expectedCodeReturned == lastResponseStatusCode
+}
+
+Then(~/^the response contains "(.*)" items$/) { int expectedAmount ->
+    List<Map> returnedList = (List<Map>) HttpConnector.getResponse().list
+    assert expectedAmount == returnedList.size()
 }
